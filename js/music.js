e4129b56252e78140e3d97b75a4773ff50dce6f2
hsBUPT <95456277+hsBUPT@users.noreply.github.com>
2023-09-29 12:44:51 +0800

    Update music.js

--- js/music.js
+++ js/music.js
@@ -10,8 +10,8 @@ GitHub：https://github.com/imsyy/home
 版权所有，请勿删除
 */
 let server = "netease"; //netease: 网易云音乐; tencent: QQ音乐; kugou: 酷狗音乐; xiami: 虾米; kuwo: 酷我
-let type = "playlist"; //song: 单曲; playlist: 歌单; album: 唱片
-let id = "8454965941"; //封面 ID / 单曲 ID / 歌单 ID
+let type = "song"; //song: 单曲; playlist: 歌单; album: 唱片
+let id = "27853883"; //封面 ID / 单曲 ID / 歌单 ID
 
 $.ajax({
     url: "https://api.wuenci.com/meting/api/?server=" + server + "&type=" + type + "&id=" + id,
